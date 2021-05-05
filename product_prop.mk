eef101db6b7d2278123984788f77033933fb48b8
Foxtrot47 <jjneutron@outlook.com>
2021-05-05 09:45:27 +0530

    vayu: props: use a tiny value for volume steps

--- product_prop.mk
+++ product_prop.mk
@@ -14,8 +14,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.media_vol_default=10 \
-	ro.config.media_vol_steps=150 \
-	ro.config.vc_call_vol_steps=11
+	ro.config.media_vol_steps=8 \
+	ro.config.vc_call_vol_steps=8
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
